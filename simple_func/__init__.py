b0db55fce61537227f5e4bb6fa039f783cca7719
SameerArif64 <138876471+SameerArif64@users.noreply.github.com>
2025-03-13 17:22:57 +0500

    Update __init__.py

--- simple_func/__init__.py
+++ simple_func/__init__.py
@@ -1,3 +1,3 @@
-from .simple_func import import_from_file, open_file, wait_for_file_availability, remove_special_characters
+from .simple_func import get_mouse_coordinates, import_from_file, open_file, select_active_window, wait_for_file_availability, remove_special_characters
 
-__all__ = ["import_from_file", "open_file", "wait_for_file_availability", "remove_special_characters"]
+__all__ = ["get_mouse_coordinates", "import_from_file", "open_file", "select_active_window", "wait_for_file_availability", "remove_special_characters"]
